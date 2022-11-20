af904479e0f43e4cfb847e5a44736c16ebe8af3c
youredith <110220308+BornInTheMiddleOfUniverse@users.noreply.github.com>
2022-11-20 22:35:49 +0900

    Add a conference form

--- dev-conf/pages/index.tsx
+++ dev-conf/pages/index.tsx
@@ -1,41 +1,55 @@
-import Head from 'next/head'
-import clientPromise from '../lib/mongodb'
-import { InferGetServerSidePropsType } from 'next'
+import { useState, useEffect, ChangeEvent } from "react";
+import { InferGetServerSidePropsType } from "next";
+import stackData from "../../tech_stack.json";
+import Seo from "../components/Seo";
 
-export async function getServerSideProps(context) {
-  try {
-    await clientPromise
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
-
-    return {
-      props: { isConnected: true },
-    }
-  } catch (e) {
-    console.error(e)
-    return {
-      props: { isConnected: false },
-    }
-  }
+interface StackData {
+  category: string;
+  category_id: string;
+  name: string;
+  tech_stack_num: number;
+  tech_stack_id: string;
 }
 
-export default function Home({
-  isConnected,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+let currCategoryId = "";
+
+export default function Home() {
+  const [category, setCategory] = useState<StackData[]>([]);
+  const [categoryId, setCategoryId] = useState("");
+  const [stacks, setStacks] = useState<StackData[]>(stackData);
+
+  useEffect(() => {
+    const getCategory = async () => {
+      setCategory(stackData);
+    };
+    getCategory();
+  }, []);
+
+  const handleCategory = (event: ChangeEvent<HTMLSelectElement>) => {
+    currCategoryId = event.target.value;
+
+    setCategoryId(currCategoryId);
+  };
+
+  useEffect(() => {
+    const getStack = async (currCategoryId: string) => {
+      let resultStacks = [];
+      if (currCategoryId !== "") {
+        resultStacks = stackData.filter((e) => {
+          return e.category_id === currCategoryId;
+        });
+      } else {
+        resultStacks = stackData;
+      }
+
+      setStacks(resultStacks);
+    };
+    getStack(currCategoryId);
+  }, [categoryId]);
+
   return (
     <div className="container">
-
-      <Head>
-        <title>
-          DevConf
-        </title>
-      </Head>
+      <Seo title={Home} />
 
       <main>
         <div className="box">
@@ -48,211 +62,61 @@ export default function Home({
             </div>
             <div>
               <label htmlFor="date">날짜</label>
-              <input id = "start-date" type="date" />
+              <input id="start-date" type="date" />
               <span> ~ </span>
-              <input id = "end-date" type="date" />
+              <input id="end-date" type="date" />
             </div>
             <div>
-              <label htmlFor="description">내용</label>            
-              <input id = "description" type="text" />
-            </div>            
+              <label htmlFor="location">위치</label>
+              <input id="location" />
+            </div>
+            <div>
+              <label htmlFor="description">내용</label>
+              <input id="description" type="text" />
+            </div>
             <div>
               <label htmlFor="site-link">사이트</label>
-              <input id = "site-link" type="url" />
+              <input id="site-link" type="url" />
             </div>
             <div>
               <label htmlFor="tech-stack">기술 스택</label>
-              <input id = "tech-stack"/>              
+              <div id="tech-stack">
+                <label htmlFor="category">카테고리</label>
+                <select id="category" onChange={(e) => handleCategory(e)}>
+                  <option>--카테고리 선택--</option>
+                  {category
+                    .filter(
+                      (arr, index, cb) =>
+                        index ===
+                        cb.findIndex((e) => e.category === arr.category)
+                    )
+                    .map((stackInfo, index) => (
+                      <option key={index} value={stackInfo.category_id}>
+                        {stackInfo.category}
+                      </option>
+                    ))}
+                </select>
+
+                <select id="stacks">
+                  <option>--스택 선택--</option>
+                  {stacks.map((getStack, index) => (
+                    <option key={index} value={getStack.tech_stack_id}>
+                      {getStack.name}
+                    </option>
+                  ))}
+                </select>
+                <input type="submit" value="추가" />
+              </div>
             </div>
 
-            <input type="submit" value="등록"/>                
-            
+            <input type="submit" value="등록" />
           </form>
-        </div>
-
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+      <footer></footer>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .subtitle {
-          font-size: 2rem;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-        
-        .box {
-
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
+      <style jsx>{``}</style>
 
       <style jsx global>{`
         html,
@@ -269,5 +133,5 @@ export default function Home({
         }
       `}</style>
     </div>
-  )
+  );
 }
